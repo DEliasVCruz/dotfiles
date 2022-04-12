bdf2260a0789b2b6255013c291ebc1431381f5e2
DEliasVilelaC <danielvilelayo@gmail.com>
2022-04-12 02:09:59 -0500

    feat(zsh): add fuzzy grep function

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -11,3 +11,10 @@ function p() {
 function tree() {
 	command tre "$@" -e && source "/tmp/tre_aliases_$USER" 2>/dev/null
 }
+
+function fzg() {
+	local result=$(rg --line-number "$@" | fzf) || exit
+	local line_num=$(echo "$result" | cut -d':' -f2)
+	local file_name=$(echo "$result" | cut -d':' -f1)
+	nvim +"$line_num" "$file_name"
+}
