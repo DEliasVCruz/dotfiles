5640e03b1e6b71ee3d279bedc8f7d6711861a39c
DEliasVilelaC <danielvilelayo@gmail.com>
2022-04-20 17:38:28 -0500

    feat(install): add more python versions installs

--- install.sh
+++ install.sh
@@ -168,8 +168,13 @@ install_languages() {
 	eval "$(pyenv init --path)" && echo "Successfully initialized pyenv path"
 	eval "$(pyenv init -)" && echo "Successfully initialized pyenv"
 
-	echo "Installing python 3.8.13"
-	pyenv install -v 3.8.13
+	echo "Installing python 3.10.4"
+	pyenv install -v 3.10.4
+	echo "Installig more latest python versions"
+	pyenv install 3.8.13
+	pyenv install 3.9.12
+	pyenv install 3.11.0a6
+	echo "Making python 3.8.13 the global version"
 	pyenv global 3.8.13
 	command -v python && echo "Successfully installed python"
 
