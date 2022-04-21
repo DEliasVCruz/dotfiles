da3b41495dd79afc1b62fe6c6f4e73085615179c
DEliasVilelaC <danielvilelayo@gmail.com>
2022-04-20 21:46:30 -0500

    feat(install): add creation of notes directory

--- install.sh
+++ install.sh
@@ -94,8 +94,8 @@ clone() {
 create_dir_structure() {
 	mkdir -p "$HOME"/.config "$XDG_DATA_HOME"/backgrounds
 	mkdir -p "$CARGO_HOME"/bin "$ZSH_DATA"/completions "$PIPX_BIN_DIR"
-	mkdir -p "$HOME"/scripts "$HOME"/repos "$HOME"/bin
 	mkdir -p "$HOME"/Desktop "$HOME"/Downloads "$HOME"/Documents
+	mkdir -p "$HOME"/scripts "$HOME"/repos "$HOME"/bin "$HOME"/notes
 }
 
 clone_main_repos() {
