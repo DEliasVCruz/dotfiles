666f5d1672c1ee44cee8fe7adef50023d52fc9c1
DEliasVilelaC <danielvilelayo@gmail.com>
2022-05-04 09:02:34 -0500

    feat(install): add iwd network backend

--- install.sh
+++ install.sh
@@ -22,7 +22,9 @@ base_system() {
 	printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost" >/etc/hosts
 	printf "\n127.0.1.1\tdevc.localdomain devc\n" >>/etc/hosts
 	pacman -S --noconfirm networkmanager networkmanager-runit
+	pacman -S --noconfirm iwd iwd-runit
 	ln -s /etc/runit/sv/NetworkManager /etc/runit/runsvdir/default
+	printf "[device]\nwifi.backend=iwd\n" >/etc/NetworkManager/conf.d/wifi_backend.conf
 }
 
 configure_pacman() {
