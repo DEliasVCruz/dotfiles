fdf157ca9d98005e67616d3e99fb389af016db3d
DEliasVilelaC <danielvilelayo@gmail.com>
2022-08-14 15:46:54 -0500

    feat(zsh): add exercism dir change function wrap

--- zsh/.config/zsh/fn.zsh
+++ zsh/.config/zsh/fn.zsh
@@ -22,3 +22,12 @@ function fzg() {
 function clipcopy() {
 	clip.exe <"${1:-/dev/stdin}"
 }
+
+function exercism() {
+	local out
+	readarray -t out < <(command exercism "$@")
+	printf '%s\n' "${out[@]}"
+	if [[ $1 == "download" && -d "${out[-1]}" ]]; then
+		cd "${out[-1]}" || return 1
+	fi
+}
